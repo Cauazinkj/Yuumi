9cbc74bcffc728a5a50f955f65a67e970ff5f1c2
Cauazinkj <cauadepaulaneves@outlook.com>
2025-11-26 19:46:22 -0300

    feat: configurei o banco de dados e o 'docker', embora nao esteja 100%, fiz um mini model de users

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+    SECRET_KEY: str = Field(..., env="SECRET_KEY")
+    API_VERSION: str = Field("v1", env="API_VERSION")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
